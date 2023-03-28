cded6b2aeeda89ecec95e49d2a525e44fcbd9a02
Mathieu <hmathieutsialonina@gmail.com>
2023-03-28 14:16:25 -0500

    add sidebar and humberger menu

--- src/components/Projects/Myproject.tsx
+++ src/components/Projects/Myproject.tsx
@@ -1,28 +1,29 @@
-import "./style.css";
-import React, { FC } from "react";
-import { Link, useParams } from "react-router-dom";
-import ProjectLists from "./ProjectLists/ProjectLists";
-import ProjectDetails from "./ProjectDetail/ProjectDetail";
+import './style.css'
+import React, { FC } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import ProjectLists from './ProjectLists/ProjectLists'
+import ProjectDetails from './ProjectDetail/ProjectDetail'
 
 const MyProject: FC = () => {
-  const params = useParams();
-  const search = new URLSearchParams(params);
+  const params = useParams()
+  const search = new URLSearchParams(params)
 
-  const projectName = search.get("name") || null;
+  const projectName = search.get('name') || null
 
   return (
     <div className="containerProject">
-      <h3> 
-        <Link to="/" style={{textDecoration: 'none'}}>PROJECTS</Link> {projectName && <span style={{fontWeight:'bold'}}>{`/ ${projectName}`}</span>}
+      <h3>
+        <Link to="/" style={{ textDecoration: 'none' }}>
+          PROJECTS
+        </Link>{' '}
+        {projectName && (
+          <span style={{ fontWeight: 'bold' }}>{`/ ${projectName}`}</span>
+        )}
       </h3>
 
-      {!projectName ? (
-        <ProjectLists/>
-      ) : (
-        <ProjectDetails />
-      )}
+      {!projectName ? <ProjectLists /> : <ProjectDetails />}
     </div>
-  );
-};
+  )
+}
 
-export default MyProject;
+export default MyProject
