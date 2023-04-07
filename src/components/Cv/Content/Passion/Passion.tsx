75b2d319e80e7c9cc103aa5cd540eb10ad1a0e9f
Mathieu <hmathieutsialonina@gmail.com>
2023-04-07 16:05:41 -0500

    add css animation, transition to passion gaming

--- src/components/Cv/Content/Passion/Passion.tsx
+++ src/components/Cv/Content/Passion/Passion.tsx
@@ -1,7 +1,10 @@
 import { FC } from 'react'
+import Techno from '../../../../assets/tech1.png'
 import Manette from '../../../../assets/manette.png'
 import Musique from '../../../../assets/musique.png'
-import Techno from '../../../../assets/tech1.png'
+import Dota from '../../../../assets/dota.png'
+import Pubg from '../../../../assets/pubg.png'
+import Fifa from '../../../../assets/fifa.png'
 
 const Passion: FC = () => {
   return (
@@ -9,10 +12,32 @@ const Passion: FC = () => {
       <h3>Passions</h3>
       <div className="passion-content">
         <div className="passion-item game-card">
-          <img src={Manette} width="250px" alt="manette" />
+          <img
+            src={Manette}
+            className="passion-logo logo-gaming"
+            alt="manette"
+          />
+          <div className="passion-detail">
+            <div>
+              <img src={Dota} className="game-icon" alt="manette" />
+            </div>
+            <div>
+              <img src={Pubg} className="pubg-icon" alt="manette" />
+            </div>
+            <div>
+              <img src={Fifa} className="fifa-icon" alt="manette" />
+            </div>
+          </div>
         </div>
         <div className="passion-item musique-card">
-          <img src={Musique} width="250px" alt="manette" />
+          <img
+            src={Musique}
+            className="passion-logo logo-musique"
+            alt="manette"
+          />
+          <div className="passion-detail">
+            <div>Spotify</div>
+          </div>
         </div>
         <div className="passion-item technologie-card">
           <img src={Techno} width="250px" alt="manette" />
