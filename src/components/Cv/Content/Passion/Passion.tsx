1c452b6ba0353382932b150a6fbaa7c6631bca55
TSIALONINA Heriniaina Mathieu <hmathieutsialonina@gmail.com>
2023-10-25 21:38:06 +0300

    fix picture on cv passion

--- src/components/Cv/Content/Passion/Passion.tsx
+++ src/components/Cv/Content/Passion/Passion.tsx
@@ -1,14 +1,14 @@
 import { FC } from 'react'
-// import JDN from '../../../../assets/jdn.jpg'
-// import Dota from '../../../../assets/dota.png'
-// import Pubg from '../../../../assets/pubg.png'
-// import Fifa from '../../../../assets/fifa.png'
-// import Techno from '../../../../assets/tech1.png'
-// import Futura from '../../../../assets/futura.svg'
-// import Deezer from '../../../../assets/deezer.png'
-// import Musique from '../../../../assets/musique.png'
-// import Manette from '../../../../assets/manette.png'
-// import Spotify from '../../../../assets/spotify.png'
+import JDN from '../../../../assets/jdn.jpg'
+import Dota from '../../../../assets/dota.png'
+import Pubg from '../../../../assets/pubg.png'
+import Fifa from '../../../../assets/fifa.png'
+import Techno from '../../../../assets/tech1.png'
+import Futura from '../../../../assets/futura.svg'
+import Deezer from '../../../../assets/deezer.png'
+import Musique from '../../../../assets/musique.png'
+import Manette from '../../../../assets/manette.png'
+import Spotify from '../../../../assets/spotify.png'
 
 interface PassionProps {
   refer: React.MutableRefObject<null>
@@ -19,20 +19,20 @@ const Passion: FC<PassionProps> = (props) => {
 
   const gamingData = [
     {
-      // img: Dota,
-      img: undefined,
+      img: Dota,
+      // img: undefined,
       name: 'dota 2',
       url: 'https://www.dota2.com/home',
     },
     {
-      // img: Pubg,
-      img: undefined,
+      img: Pubg,
+      // img: undefined,
       name: 'pubg',
       url: 'https://play.battlegrounds.pubg.com/',
     },
     {
-      // img: Fifa,
-      img: undefined,
+      img: Fifa,
+      // img: undefined,
       name: 'fifa',
       url: 'https://www.ea.com/fr-fr/games/fifa',
     },
@@ -40,14 +40,14 @@ const Passion: FC<PassionProps> = (props) => {
 
   const musicData = [
     {
-      // img: Spotify,
-      img: undefined,
+      img: Spotify,
+      // img: undefined,
       name: 'spotify',
       url: 'https://open.spotify.com/?',
     },
     {
-      // img: Deezer,
-      img: undefined,
+      img: Deezer,
+      // img: undefined,
       name: 'deezer',
       url: 'https://www.deezer.com/',
     },
@@ -55,14 +55,14 @@ const Passion: FC<PassionProps> = (props) => {
 
   const technoData = [
     {
-      // img: Futura,
-      img: undefined,
+      img: Futura,
+      // img: undefined,
       name: 'futura',
       url: 'https://www.futura-sciences.com/tech/actualites/',
     },
     {
-      // img: JDN,
-      img: undefined,
+      img: JDN,
+      // img: undefined,
       name: 'JDN',
       url: 'https://www.journaldunet.com/',
     },
@@ -74,8 +74,8 @@ const Passion: FC<PassionProps> = (props) => {
       <div className="passion-content">
         <div className="passion-item game-card">
           <img
-            // src={Manette}
-            src={undefined}
+            src={Manette}
+            // src={undefined}
             className="passion-logo logo-gaming"
             alt="manette"
           />
@@ -91,8 +91,8 @@ const Passion: FC<PassionProps> = (props) => {
         </div>
         <div className="passion-item musique-card">
           <img
-            // src={Musique}
-            src={undefined}
+            src={Musique}
+            // src={undefined}
             alt="manette"
             className="passion-logo logo-musique"
           />
@@ -112,8 +112,8 @@ const Passion: FC<PassionProps> = (props) => {
         </div>
         <div className="passion-item technologie-card">
           <img
-            // src={Techno}
-            src={undefined}
+            src={Techno}
+            // src={undefined}
             width="250px"
             alt="manette"
             className="logo-techno"
