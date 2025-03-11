9094e3c7801c4aa5cc2c87e7e64052976a38aa48
TSIALONINA Heriniaina Mathieu <hmathieutsialonina@gmail.com>
2025-03-11 11:18:39 +0300

    update contact info

--- src/common/contact/Contact.tsx
+++ src/common/contact/Contact.tsx
@@ -1,19 +1,21 @@
-import Link from "../../components/Link";
 import "./contact.css";
 import { FC } from "react";
+import Link from "../../components/Link";
 
 interface contactProp {
   icon: any;
   link: string;
+  content?: string;
 }
 
 const Contact: FC<contactProp> = (props) => {
-  const { icon, link } = props;
+  const { icon, link, content } = props;
 
   return (
-    <div className="contactContainer">
+    <div className="contactContainer" id={link}>
       <Link target={link}>
-        <img src={icon} width={25} height={25} alt="contact" loading="lazy" />
+        <img src={icon} width={20} height={20} alt="contact" loading="lazy" />
+        <label htmlFor={link} className="label-link">{content}</label>
       </Link>
     </div>
   );
